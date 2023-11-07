c3002a8cc7f198d6cf9cf22c9f32640e3a5c58a5
Brett Heinkel <bheinks@gmail.com>
2023-11-06 20:25:14 -0600

    Fix docker vars

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('POSTGRES_DB'),
-        'USER': config('PGUSER'),
+        'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('DB_HOST'),
     }
